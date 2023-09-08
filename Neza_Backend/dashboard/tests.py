f107c0be868f87d0851dfe535a7e23d90eed0eec
NjeriMarion <njeriwmarion@gmail.com>
2023-09-08 09:52:44 +0300

    ran tests for the dashboard model

--- Neza_Backend/dashboard/tests.py
+++ Neza_Backend/dashboard/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Dashboard
 
-# Create your tests here.
+class DashboardModelTest(TestCase):
+    def setUp(self):
+        self.dashboard = Dashboard.objects.create(
+            location="Test Location",
+            no_of_industries=5,
+            is_borehole=True,
+            population=1000,
+        )
+
+    def test_dashboard_str_method(self):
+        expected_str = "Test Location"
+
+
+    def test_default_values(self):
+        dashboard = Dashboard.objects.create(location="Nairobi", no_of_industries=5, is_borehole=True, population=1000,)
+        self.assertEqual(dashboard.no_of_industries, 5)
+        self.assertEqual(dashboard.is_borehole, True)
+        self.assertEqual(dashboard.population, 1000)
+
+    def test_model_fields(self):
+        self.assertIsInstance(self.dashboard.location, str)
+        self.assertIsInstance(self.dashboard.no_of_industries, int)
+        self.assertIsInstance(self.dashboard.is_borehole, bool)
+        self.assertIsInstance(self.dashboard.population, int)
