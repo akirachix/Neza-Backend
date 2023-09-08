a7b7334166a681d383100aa26e3326c3c73d13c4
NjeriMarion <njeriwmarion@gmail.com>
2023-09-08 18:34:58 +0300

    made changes to the the dashboard  model

--- Neza_Backend/api/tests.py
+++ Neza_Backend/api/tests.py
@@ -8,9 +8,11 @@ from rest_framework import status
 class DashboardViewTest(TestCase):
     def setUp(self):
         self.client = APIClient()
+        
     def test_dashboard_list_view(self):
         response = self.client.get('dashboard/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual('Dashboard successfully displayed',response.status_code, status.HTTP_200_OK)
+        
     def test_dashboard_detail_view(self):
         response = self.client.get(f'dashboarddetails/<int:id>/{self.id}/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual('Details successfully displayed',response.status_code, status.HTTP_200_OK)
