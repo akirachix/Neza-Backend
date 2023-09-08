14428368d62f8202553ab0039bff87095d6b5086
Student <mwauloice@gmail.com>
2023-09-08 20:33:32 +0300

    made changes to the views file

--- Neza_Backend/Neza_Backend/settings.py
+++ Neza_Backend/Neza_Backend/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stagetracking',
-    'rest_framework',
-    'api',
 ]
 
 MIDDLEWARE = [
--- Neza_Backend/api/views.py
+++ Neza_Backend/api/views.py
@@ -8,34 +8,46 @@ from rest_framework import status
 # Create your views here.
 class StageTrackingListView(APIView):
     def get(self, request):
-        stagetracking =OrganizationStageTracking.objects.all()  # Corrected variable name
+        stagetracking =OrganizationStageTracking.objects.all() 
         serializer = StageTrackingSerializer(stagetracking, many=True)
         return Response(serializer.data)
     def post(self,request):
         serializer=StageTrackingSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response("Stage tracking created successfully",status=status.HTTP_201_CREATED)
         
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response("error while created stage tracking",status=status.HTTP_400_BAD_REQUEST)
 
 class StageTrackingDetailView(APIView):
-    def get(self,request,id,format=None):
-        stagetracking=OrganizationStageTracking.objects.get(id=id)
-        serializer=StageTrackingSerializer(stagetracking)
-        return Response(serializer.data)
+    def get(self, request, id, format=None):
+        try:
+            stagetracking = OrganizationStageTracking.objects.get(id=id)
+            serializer = StageTrackingSerializer(stagetracking)
+            return Response(serializer.data)
+        except OrganizationStageTracking.DoesNotExist:
+            return Response("Stage tracking not found", status=status.HTTP_404_NOT_FOUND)
+    
     
     def put(self,request,id,format=None):
-        customer=OrganizationStageTracking.objects.get(id=id)
-        serializer=StageTrackingSerializer(customer,request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+         try:
+            stagetracking = OrganizationStageTracking.objects.get(id=id)
+            serializer = StageTrackingSerializer(stagetracking, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response("Stage tracking updated successfully", status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+         except OrganizationStageTracking.DoesNotExist:
+            return Response("Stage tracking not found", status=status.HTTP_404_NOT_FOUND)
+       
     
     def delete(self,request,id,format=None):
-        customer=OrganizationStageTracking.objects.get(id=id)
-        customer.delete()
-        return Response("stage tracking successfully deleted",status=status.HTTP_204_NO_CONTENT)
+        try:
+            stagetracking = OrganizationStageTracking.objects.get(id=id)
+            stagetracking.delete()
+            return Response("Stage tracking successfully deleted", status=status.HTTP_204_NO_CONTENT)
+        except OrganizationStageTracking.DoesNotExist:
+            return Response("Stage tracking not found", status=status.HTTP_404_NOT_FOUND)
+        
     
 
