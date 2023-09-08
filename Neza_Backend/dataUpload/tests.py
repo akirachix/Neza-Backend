d08b96345c1c03fda0c0f29794d1d518680153c0
Grace kazungu <kazungungrace@gmail.com>
2023-09-08 12:02:33 +0300

    Added Tests to the Neza-Backend api app

--- Neza_Backend/dataUpload/tests.py
+++ Neza_Backend/dataUpload/tests.py
@@ -3,15 +3,6 @@ from .models import DataUpload
 
 # # Create your tests here.
 
-# class DataUploadTestCase(TestCase):
-#     def setUp(self):
-#         DataUpload.objects.create(file="/media/uploads/neza21.csv",file_name="test_file.txt", file_type="txt", file_upload_status="uploaded")
-
-#     def test_data_upload_creation(self):
-#         data_upload = DataUpload.objects.get(file_name="test_file.txt")
-#         self.assertEqual(data_upload.file_type, 'txt')
-#         self.assertEqual(data_upload.file_upload_status, 'uploaded')
-
 from django.test import TestCase
 from django.utils import timezone
 from .models import DataUpload
