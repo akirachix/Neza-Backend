2295847be93fe7028a2587d53e206c46c4f795e2
Grace kazungu <kazungungrace@gmail.com>
2023-09-08 11:53:33 +0300

    Added Tests to the Neza-Backend api app

--- Neza_Backend/api/tests.py
+++ Neza_Backend/api/tests.py
@@ -1,59 +1,60 @@
 from django.test import TestCase
-from rest_framework.test import APIClient
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APIClient
 from dataUpload.models import DataUpload
-from .serializers import DataUploadSerializer
+from api.serializers import DataUploadSerializer
 
-class DataUploadAPITest(TestCase):
+class DataUploadListViewTest(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-        self.data_upload1 = DataUpload.objects.create(
-            file_name='file1.txt',
-            file_type='txt',
-            file_upload_status='uploaded'
-        )
-        self.data_upload2 = DataUpload.objects.create(
-            file_name='file2.csv',
-            file_type='csv',
-            file_upload_status='processing'
-        )
-
-    def test_get_datauploads(self):
-        response = self.client.get('/api/datauploads/')  
+    def test_get_data_upload_list(self):
+        url = reverse('data_upload_list_view')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        expected_data = DataUploadSerializer([self.data_upload1, self.data_upload2], many=True).data
-        self.assertEqual(response.data, expected_data)
-
-    def test_create_dataupload(self):
+    def test_create_data_upload(self):
+        url = reverse('data_upload_list_view')
+        file = SimpleUploadedFile("test_file.csv", b"file_content", content_type="text/csv")
         data = {
-            'file_name': 'new_file.txt',
-            'file_type': 'txt',
+            'file': file,
+            'file_name': 'Nakurucase.csv',
+            'file_type': 'csv',
             'file_upload_status': 'uploaded'
         }
-
-        response = self.client.post('/api/datauploads/', data, format='json')
-
+        response = self.client.post(url, data, format='multipart')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        self.assertTrue(DataUpload.objects.filter(file_name='new_file.txt').exists())
+class DataUploadDetailViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.data_upload = DataUpload.objects.create(
+            file_name="Nakurucase.csv",
+            file_type="csv",
+            file_upload_status="uploaded",
+            file=SimpleUploadedFile("test_file.csv", b"file_content", content_type="text/csv")
+        )
 
-    def test_update_dataupload(self):
-        data = {
-            'file_upload_status': 'updated'
-        }
-
-        response = self.client.put(f'/api/datauploads/{self.data_upload1.id}/', data, format='json')
+    def test_get_data_upload_detail(self):
+        url = reverse('data_upload_detail_view', args=[self.data_upload.id])
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        updated_data_upload = DataUpload.objects.get(id=self.data_upload1.id)
-        self.assertEqual(updated_data_upload.file_upload_status, 'updated')
+    def test_update_data_upload(self):
+        url = reverse('data_upload_detail_view', args=[self.data_upload.id])
+        file = SimpleUploadedFile("updated_file.csv", b"updated_content", content_type="text/csv")
+        data = {
+            'file': file,
+            'file_name': 'Updated_Nakurucase.csv',
+            'file_type': 'csv',
+            'file_upload_status': 'updated'
+        }
+        response = self.client.put(url, data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_delete_dataupload(self):
-        response = self.client.delete(f'/api/datauploads/{self.data_upload1.id}/')
-
+    def test_delete_data_upload(self):
+        url = reverse('data_upload_detail_view', args=[self.data_upload.id])
+        response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-        self.assertFalse(DataUpload.objects.filter(id=self.data_upload1.id).exists())
-
