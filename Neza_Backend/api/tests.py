3eedd61869cc704f96eee11bd1ac5497796385c3
NjeriMarion <njeriwmarion@gmail.com>
2023-09-13 17:33:09 +0300

    updated unit tests

--- Neza_Backend/api/tests.py
+++ Neza_Backend/api/tests.py
@@ -1,15 +1,9 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
-from dashboard.models import Dashboard
-from .serializers import DashboardSerializer
-from rest_framework import status
 
-# Create your tests here.
-class DashboardViewTest(TestCase):
+class DashboardListViewTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
         
-    def test_dashboard_list_view(self):
-        response = self.client.get('dashboard_location_details/')
-        self.assertEqual('Dashboard successfully displayed',response.status_code, status.HTTP_200_OK)
-                
+    def test_get_dashboard_list(self):
+        response = self.client.get('/api/dashboard/')
--- Neza_Backend/dashboard/tests.py
+++ Neza_Backend/dashboard/tests.py
@@ -4,18 +4,18 @@ from .models import Dashboard
 class DashboardModelTest(TestCase):
     def setUp(self):
         self.dashboard = Dashboard.objects.create(
-            location="Test Location",
+            location="Nairobi",
             no_of_industries=5,
             is_borehole=True,
             population=1000,
         )
 
     def test_dashboard_str_method(self):
-        expected_str = "Test Location"
+        expected_str = "Nairobi"
 
 
     def test_default_values(self):
-        dashboard = Dashboard.objects.create(location="Nairobi", no_of_industries=5, is_borehole=True, population=1000,)
+        dashboard = Dashboard.objects.create(location="Nairobi", no_of_industries=5, is_borehole=True, population=1000)
         self.assertEqual(dashboard.no_of_industries, 5)
         self.assertEqual(dashboard.is_borehole, True)
         self.assertEqual(dashboard.population, 1000)
