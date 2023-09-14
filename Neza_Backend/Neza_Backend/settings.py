e7330bea8a6c3f23438dfce94125082a53c121ab
Student <mwauloice@gmail.com>
2023-09-14 20:08:52 +0300

    added unique identifier

--- Neza_Backend/Neza_Backend/settings.py
+++ Neza_Backend/Neza_Backend/settings.py
@@ -97,12 +97,12 @@ WSGI_APPLICATION = 'Neza_Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'one',
-        'USER': 'postgres',
-        'PASSWORD': 'newpassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     },
 }
 
