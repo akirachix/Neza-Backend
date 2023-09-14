07a7fb8651f277d7deae07235d01432f673534c6
Grace kazungu <kazungungrace@gmail.com>
2023-09-14 10:31:53 +0300

    made changes

--- Neza_Backend/api/tests.py
+++ Neza_Backend/api/tests.py
@@ -1,45 +1,24 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
+
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from dataUpload.models import ExtractedData
+from .serializers import ExtractedDataSerializer
+from django.http import Http404
 
-class ExtractedDataTests(TestCase):
-    def setUp(self):
-        self.client = APIClient()
+class ExtractedDataDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return ExtractedData.objects.get(pk=pk)
+        except ExtractedData.DoesNotExist:
+            raise Http404
 
-    def test_upload_file(self):
-        test_csv_data = "location,sources of water,proximity to industries,number of garages in an area," \
-                        "proximity to dumpsite,presence of open sewage,past cases of lead poisoning,women and children population\n" \
-                        "Test Location,Yes,123,45,Yes,No,0,678\n"
-        
-        response = self.client.post('/upload_file/', {'file': test_csv_data}, format='multipart')
+    def get(self, request, pk):
+        extracted_data = self.get_object(pk)
+        serializer = ExtractedDataSerializer(extracted_data)
+        return Response(serializer.data)
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(ExtractedData.objects.count(), 1)
-        extracted_data = ExtractedData.objects.first()
-        self.assertEqual(extracted_data.location, "Test Location")
-
-    def test_extracted_data_list_view(self):
-        ExtractedData.objects.create(location="Location 1", sources_of_water=1)
-        ExtractedData.objects.create(location="Location 2", sources_of_water=0)
-
-        response = self.client.get('/extracted-data/')
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
-
-    def test_extracted_data_detail_view(self):
-        extracted_data = ExtractedData.objects.create(location="Location 1", sources_of_water=1)
-
-        response = self.client.get(f'/extracted-data/{extracted_data.id}/')
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['location'], "Location 1")
-
-    def test_extracted_data_delete_view(self):
-        extracted_data = ExtractedData.objects.create(location="Location 1", sources_of_water=1)
-
-        response = self.client.delete(f'/extracted-data/{extracted_data.id}/')
-
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(ExtractedData.objects.count(), 0)
+    def delete(self, request, pk):
+        extracted_data = self.get_object(pk)
+        extracted_data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
