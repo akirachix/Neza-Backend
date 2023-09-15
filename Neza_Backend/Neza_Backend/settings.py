21fb9266358c22cb77d82765487fb87ca6f33139
Grace kazungu <kazungungrace@gmail.com>
2023-09-15 11:19:27 +0300

    added .env to gitignore

--- Neza_Backend/Neza_Backend/settings.py
+++ Neza_Backend/Neza_Backend/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'Neza_Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': os.environ.get('DB_ENGINE'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
