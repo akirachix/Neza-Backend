979c2dd56c9bb7225f9571fed1b854459e168858
NjeriMarion <njeriwmarion@gmail.com>
2023-10-01 15:04:47 +0300

    changes

--- api/views.py
+++ api/views.py
@@ -38,10 +38,7 @@ from dashboard.models import Dashboard
 from .serializers import DashboardSerializer
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import JsonResponse
-import joblib
-import json
-from django.views.decorators.csrf import csrf_exempt
-import pandas as pd
+
 
 
 # account views
