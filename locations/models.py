9fa9de28e0a31011177745f117bb39120dd9b128
NjeriMarion <njeriwmarion@gmail.com>
2023-10-06 01:11:20 +0300

    locations

--- locations/models.py
+++ locations/models.py
@@ -8,7 +8,7 @@ class Locations(models.Model):
     name = models.CharField(max_length=100)
     lat = models.FloatField()
     lng = models.FloatField()
-    percentage = models.FloatField(null=True, blank=True)
+    # percentage = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.name
